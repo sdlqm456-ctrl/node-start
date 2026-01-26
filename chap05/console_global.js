d2ec5dd2ac4e3f2195e63c55a19fdcd884eab433
sdlqm456-ctrl <sdlqm456@gmail.com>
2026-01-26 23:49:50 +0900

    Update console_global.js

--- chap05/console_global.js
+++ chap05/console_global.js
@@ -1,12 +1,15 @@
+// 시간 측정
 console.time("time check");
 for (let i = 1; i <= 10000; i++) {}
-console.timeEnd("time check");
+console.timeEnd("time check"); // for 반복문의 실행시간을 출력
 
 console.log(`Hello, Choi`);
-console.error("Error 발생");
+console.error("Error 발생"); // 에러메세지 출력
 
+// 배열을 표로 출력하기
 const ary = [
   { name: "홍길동", age: 20 },
   { name: "박길동", age: 25 },
 ];
 console.table(ary);
+
